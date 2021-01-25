3526b81fa462ac2b55dcdd49d9ceb04b6969b2fd
kudakwashe <kuda@nightsbridge.co.za>
2021-01-25 20:50:39 +0200

    Added Payments Model

--- src/main/java/com/nyamita/paymentgateway/payment/api/model/Payment.java
+++ src/main/java/com/nyamita/paymentgateway/payment/api/model/Payment.java
@@ -1,7 +1,39 @@
 package com.nyamita.paymentgateway.payment.api.model;
 
-public class Payment {
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "payments", indexes = {@Index(name = "indx_payment", columnList = "id", unique = true)})
+@Getter
+@Setter
+@ToString
+@Access(AccessType.FIELD)
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+
+public class Payment {
+  @Id
+  private Long id;
+  @Column(name = "paymentid", nullable = false, length = 45)
   private String paymentId;
-  private String paymentAmount;
+  @Column(name = "paymentamount", nullable = false)
+  private double paymentAmount;
+  @Column(name = "paymentreference", nullable = false, length = 45)
+  private String paymentReference;
+  @Column(name = "paymentdate", nullable = false)
+  private LocalDate paymentDate;
+  @Column(name = "accountholder", nullable = false)
+  private  String accountHolder;
+  @Column(name = "accountnumber", nullable = false)
+  private String accountNumber;
+  @Column(name = "destinationaccount", nullable = false)
+  private String destinationAccount;
+  @Column(name = "issuersenderbank", nullable = false, length = 45)
+  private String issuerSenderBank;
+
 }
